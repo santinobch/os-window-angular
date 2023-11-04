5ea9a13d275a362323c1039e713e12a56ebb7f03
santinobch <santino.bch@gmail.com>
2023-11-04 01:27:12 -0300

    Added prettier & tailwindcss

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  trailingComma: 'es5',
+  bracketSameLine: true,
+  printWidth: 80,
+  plugins: ['prettier-plugin-tailwindcss'],
+  endOfLine: 'lf',
+};
